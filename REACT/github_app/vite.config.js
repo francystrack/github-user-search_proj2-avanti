4d2cc17f66aa34a983ea603c49b2daeca8e42c4a
Franciele Strack <francystrack@gmail.com>
2025-04-24 19:15:32 -0300

    gh pages deploy

--- REACT/github_app/vite.config.js
+++ REACT/github_app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/github-user-search_proj2-avanti"
+  base: "github-user-search_proj2-avanti"
 })
