ca54d7d506321f5e0d9b30d808fae816636be68f
Franciele Strack <francystrack@gmail.com>
2025-04-24 17:39:18 -0300

    correcting

--- REACT/github_app/src/components/GitHubUserComponents.jsx
+++ REACT/github_app/src/components/GitHubUserComponents.jsx
@@ -5,19 +5,20 @@ import styles from './CSSComponents/GitHubUserComponents.module.css'
 
 
 const GitHubUserComponents = (props) => {
-    // const token = "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";   //token para acessar a API
-    // const token = "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";
+    // const token = "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";   //token para acessar a API
     
     const [userData, setUserData] = useState(null)
     const [hasError, setHasError] = useState(false)
     
     useEffect(() => {
         if(props.username) {
-            fetch(`https://api.github.com/users/${props.username}`, {
-                // headers: new Headers({
-                //     'Authorization': `Bearer ${token}`
-                // })
-            })
+            fetch(`https://api.github.com/users/${props.username}`
+            //     , {
+            //     headers: new Headers({
+            //         'Authorization': `Bearer ${token}`
+            //     })
+            // }
+        )
             .then((response) => {
                 return response.json();
             })
